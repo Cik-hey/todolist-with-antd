16016c259af9ee87894b40de8e70ac597796a966
Cik-hey <christian.karl.calano@oocl.com>
2022-09-14 23:38:53 +0800

    refactor: added promise syntax in todolist

--- src/features/todo/TodoItem.js
+++ src/features/todo/TodoItem.js
@@ -14,6 +14,22 @@ const TodoItem = (props) =>  {
     const onToggle = () => {
         dispatch(toggleTodo(todo.id));
     };
+
+    let myPromise = new Promise(function(myResolve, myReject) {
+        if (todo.done === true) {
+          myResolve("todo.done is true");
+        } else {
+          myReject("todo.done is false");
+        }
+      });
+
+      myPromise.then(
+        function(value) {
+            console.log(value);},
+        function(error) {
+            console.log(error);}
+      );
+
   return <div className="task-item" onClick={onToggle}>
     <span className={todo.done ? "done" : ""}>{todo.text}</span>
     <span className="times" onClick={onDelete}>&times;</span>
